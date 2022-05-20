8ac75e533b5563e9d681f479a669bc6331e5c930
Paul Libbrecht <paul.libbrecht@cabri.com>
2022-05-20 20:43:39 +0200

    Multi-directory finally working.

--- src/_data/figures.js
+++ src/_data/figures.js
@@ -1,37 +1,12 @@
 const glob = require("glob");
 const fs = require("fs");
+const cg3Objects = require("../js/cg3Objects.js")
 
-function removeExtension(n) {
-  let filename = n.split('/').pop();
-  //strip off the file type, this could probably be one line of fancier JS
-  let parts = filename.split('.');
-  parts.pop();
-  return parts.join('.');
-}
+async function generateFigures(eleventyConfig) {
+  let files = glob.sync('**/*.cg3', {ignore: "_site/**"});
 
-
-async function generateFigures(configData) {
-  let files = glob.sync('./*.cg3');
   let figuresCollection = files.map(i => {
-    let fileRoot = removeExtension(i);
-    let name = (fileRoot.replace(/.*\//,""))
-    let dir = fileRoot.replace(/\/.*/, "");
-    let thumbPath = "img/Cabri3DDoc.png";
-
-    if(fs.existsSync(fileRoot + ".png")) {
-      if(!fs.existsSync("_site"))  fs.mkdirSync("_site")
-      fs.copyFileSync(fileRoot + ".png", "_site" + "/" + fileRoot + ".png")
-      thumbPath =  fileRoot + ".png";
-    }
-
-    return {
-      file: i,
-      thumbpath: thumbPath,
-      name: name,
-      title: name,
-      description: name
-
-    }
+    return cg3Objects.getCG3Info(i);
   })
   figuresCollection.name = "figures"
   return figuresCollection;
